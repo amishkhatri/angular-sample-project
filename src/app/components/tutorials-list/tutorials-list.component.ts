3f5f8ee408fd7bcaf14735e323a71a8ae2f929b8
amish.khatri <amish.khatri@gmail.com>
2023-04-25 16:32:51 +0530

    second commit

--- src/app/components/tutorials-list/tutorials-list.component.ts
+++ src/app/components/tutorials-list/tutorials-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Tutorial } from 'src/app/models/tutorial.model';
 import { TutorialService } from 'src/app/services/tutorial.service';
 
+
 @Component({
   selector: 'app-tutorials-list',
   templateUrl: './tutorials-list.component.html',
